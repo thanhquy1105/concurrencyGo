c4ae9c59a42d3619f518577a9e62e4c8ee8395ae
Thành Quý <49222110+thanhquy1105@users.noreply.github.com>
2023-10-29 16:40:51 +0700

    Update diningphilosophers_test.go

--- 3. Dining Philosophers/content/diningphilosophers_test.go
+++ 3. Dining Philosophers/content/diningphilosophers_test.go
@@ -1,1 +1,43 @@
 package diningphilosophers
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_dine(t *testing.T) {
+	eatTime = 0 * time.Second
+	sleepTime = 0 * time.Second
+	thinkTime = 0 * time.Second
+
+	for i := 0; i < 10; i++ {
+		orderFinished = []string{}
+		dine()
+		if len(orderFinished) != 5 {
+			t.Errorf("incorrect length of slice; expected 5 but got %d\n", len(orderFinished))
+		}
+	}
+}
+
+func Test_dineWithVaryingDelays(t *testing.T) {
+	var theTests = []struct {
+		name  string
+		delay time.Duration
+	}{
+		{"zero delay", time.Second * 0},
+		{"quarter second delay", time.Millisecond * 250},
+		{"half second delay", time.Millisecond * 500},
+	}
+
+	for _, e := range theTests {
+		orderFinished = []string{}
+		eatTime = e.delay
+		thinkTime = e.delay
+		sleepTime = e.delay
+
+		dine()
+		if len(orderFinished) != 5 {
+			t.Errorf("%s: incorrect length of slice; expected 5 but got %d\n", e.name, len(orderFinished))
+		}
+	}
+}
