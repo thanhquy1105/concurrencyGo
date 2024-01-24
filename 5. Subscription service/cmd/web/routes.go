033272c628303ed14e4c3bb9a047eff82a973a27
Thành Quý <49222110+thanhquy1105@users.noreply.github.com>
2024-01-25 04:08:50 +0700

    graceful shutdown

--- 5. Subscription service/cmd/web/routes.go
+++ 5. Subscription service/cmd/web/routes.go
@@ -13,6 +13,12 @@ func (app *Config) routes() http.Handler {
 	mux.Use(app.SessionLoad)
 
 	mux.Get("/", app.HomePage)
+	mux.Post("/login", app.LoginPage)
+	mux.Get("/logout", app.Logout)
+	mux.Get("/register", app.RegisterPage)
+	mux.Post("/register", app.PostRegisterPage)
+	mux.Get("/activate-account", app.ActivateAccount)
+
 	return mux
 
 }
